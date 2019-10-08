dec9c8e8e7ebdc8246162974915f48c00d26fcc7
Diptin Dahal <diptindahal1419@gmail.com>
2019-10-08 10:23:03 -0500

    Initial Working State

--- DataScraper/DataScraper.py
+++ DataScraper/DataScraper.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import json 
 
 url = 'http://ethans_fake_twitter_site.surge.sh/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
-for tweet in content.find('p', attrs={"class": "content"}):
-    print(tweet.text.encode('utf-8))
-
-
+tweetArr = []
+for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
+    #print(tweet.text.encode('utf-8'))
+    tweetObject = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text,
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
+        "tweet": tweet.find('p', attrs={"class": "content"}).text,
+        "likes": tweet.find('p', attrs={"class": "likes"}).text,
+        "shares": tweet.find('p', attrs={"class": "shares"}).text
+    }
+    tweetArr.append(tweetObject)
+with open('twitterData.json','w') as outfile:
+    json.dump(tweetArr,outfile)
 
